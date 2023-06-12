127a27a37a11101a82bcf928cdab17a3895fd6ae
Michiel <2036886+acidzebra@users.noreply.github.com>
2023-06-12 16:00:50 +0200

    1.5_b1
    - I think we broke out of the ref/radius loop too soon, was missing grass it caught before. Fixed, I think.

--- lawnmower.py
+++ lawnmower.py
@@ -1,5 +1,5 @@
 # The LawnMower for Morrowind
-version = "1.4.2"
+version = "1.5_b1"
 #
 # automatically clean all clipping grass from your Morrowind grass mods, no more grass sticking through floors and other places it doesn't belong.
 # it is a little rough and there is very little handholding or much in the way of sanity checks. But it works.
@@ -14,20 +14,19 @@ version = "1.4.2"
 # 1.2 - removed leftover debug stuff, fixed radius select loop
 # 1.3 - further simplification, reduced amount of stuff to evaluate during loops
 # 1.4 - refinement of radius lists, code cleanup and optimization, added nograss_xxl for easier city cleaning using grassblocker, added autoclean_cities_vanilla.esp for cleaning stuff in vanilla that lawnmower can't reach by itself
-# 1.4.1 - trimmed back lists a tiny bit, was getting out of hand (and increasing runtime for little extra benefit). Slight reorder of file loading to make error messages less vague, thinking of adding batch processing. Maybe in a separate script though.
-# 1.4.2 - rewrote ref lookup and saved many lines of code, removed scale stuff since it's pretty useless, now frees up memory after loading json files
-# 1.4.3 - should probably not zero out skipitem before I use it, small code fix
+# 1.5 - rewrote ref matching loops, futher refinement of radius lists, reduced memory use, minor changes to file loading, fixup of another loop bug.
+
 # START OF USER-CONFIGURABLE STUFF
 
 # moreinfo mode spits out more messages, defaults to True
 moreinfo = True
-# delete the mod .json file after generation? Default True, set to False to speed up batch operations (will be reused). 
+# delete the mod .json file after generation? Default True, set to False to speed up batch operations (json file will be reused). 
 # DON'T FORGET TO TURN THIS OFF IF YOU'VE MADE CHANGES TO A MOD IN BETWEEN RUNNING LAWNMOWER.
 deletemodjson = True
 # radius to cut grass around mesh if no overrides on basis of refID, default 220.00
 defaultradius = 220.00
 
-# radius control, the more things in these lists, the slower things go. This is a decent set and seems to catch vanilla/TR/OAAB stuff pretty well.
+# radius control, the more things in these lists, the slower things go. This is a decent set and seems to catch vanilla/TR/OAAB/etc stuff pretty well.
 skiplist = ["bridge","invis","collis","smoke","log","wreck","ship","boat","plank","light_de","sound","teleport","trigger","thiefdoor","_ward_","steam","beartrap","marker","fauna","fx","forcefield","ranched","scrib","_fau_","_cre_","cr_","lvl_","_lev+","_lev-","_cattle","_sleep","_und_","bm_ex_fel","bm_ex_hirf","bm_ex_moem","bm_ex_reav","wolf","bm_ex_isin","bm_ex_riek","kwama","crab","t_sky_stat_","t_sky_rstat","SP_stat_","berserk","terrain_rock_wg_06","terrain_rock_wg_04","terrain_rock_wg_11","terrain_rock_wg_13"]
 smalllist = ["tree","parasol","railing","flora","dwrv_block","rubble","nograss_small","plant","pole","furn"]
 largelist = ["strongh","pylon","portal","ex_velothi","entrance","_talker","entr_","terrwater","necrom","temple","fort","doomstone","lava","canton","altar","palace","tower","_keep","fire","tent","statue","nograss_large","striderport","bcom_gnisis_rock","terrain_rock_wg_09","terrain_rock_wg_10","terrain_rock_wg_12"]
@@ -35,11 +34,12 @@ mediumlist = ["ex_","house","building","shack","ruin","bw_hlaal","door","_d_","d
 xllist = ["nograss_xl"]
 xxllist = ["nograss_xxl"]
 
+# lists are extensible, you could create a new one with your custom stuff and then add the relevant data to the lists below (name of list, radius setting, whether to skip for cutting grass)
 reftable = [skiplist,smalllist,largelist,mediumlist,xllist,xxllist]
 radiustable = [1,120,160,400,1000,2000]
 skiptable = [True,False,False,False,False,False]
 
-# if you want to see what decisions are made about refs, would recommend to pipe output to a text file
+# if you want to see what decisions are made about refs set this to True, would recommend to pipe output to a text file
 debugradiuslist = False
 
 ### END OF USER-CONFIGURABLE STUFF
@@ -163,14 +163,12 @@ for keys in grassfile_parsed_json:
                                 matchitem = False
                                 tablecount = 0
                                 for letsref in reftable:
-                                    if is_in_list(checkthismesh,reftable[tablecount]):
+                                    if not matchitem and is_in_list(checkthismesh,reftable[tablecount]):
                                         skipitem = skiptable[tablecount]
-                                        radius = radiustable[tablecount]
+                                        radius = float(radiustable[tablecount])
                                         matchitem = True
                                         if debugradiuslist:
                                             print(skipitem,radius, checkthismesh)
-                                    if matchitem:
-                                        break
                                     tablecount+=1
                                 matchitem = False 
                                 if not alreadymoved and not skipitem and is_clipping(comparerefs["translation"][0],comparerefs["translation"][1],radius,refs["translation"][0],refs["translation"][1]):
