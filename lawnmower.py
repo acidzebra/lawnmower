a74b911524b1348d930c5eabfe538bedbfd49d17
Michiel <2036886+acidzebra@users.noreply.github.com>
2023-06-12 17:47:10 +0200

    wrong FILE

--- lawnmower.py
+++ lawnmower.py
@@ -1,5 +1,5 @@
 # The LawnMower for Morrowind
-version = "1.4.2"
+version = "1.5"
 #
 # automatically clean all clipping grass from your Morrowind grass mods, no more grass sticking through floors and other places it doesn't belong.
 # it is a little rough and there is very little handholding or much in the way of sanity checks. But it works.
@@ -14,7 +14,7 @@ version = "1.4.2"
 # 1.2 - removed leftover debug stuff, fixed radius select loop
 # 1.3 - further simplification, reduced amount of stuff to evaluate during loops
 # 1.4 - refinement of radius lists, code cleanup and optimization, added nograss_xxl for easier city cleaning using grassblocker, added autoclean_cities_vanilla.esp for cleaning stuff in vanilla that lawnmower can't reach by itself
-# 1.5 - rewrote ref matching loops, futher refinement of radius lists, reduced memory use, minor changes to file loading
+# 1.5 - rewrote ref matching loops + bugfixes, futher refinement of radius lists, reduced memory use, minor changes to file loading
 
 # START OF USER-CONFIGURABLE STUFF
 
@@ -22,7 +22,7 @@ version = "1.4.2"
 moreinfo = True
 # delete the mod .json file after generation? Default True, set to False to speed up batch operations (json file will be reused). 
 # DON'T FORGET TO TURN THIS OFF IF YOU'VE MADE CHANGES TO A MOD IN BETWEEN RUNNING LAWNMOWER.
-deletemodjson = False
+deletemodjson = True
 # radius to cut grass around mesh if no overrides on basis of refID, default 220.00
 defaultradius = 220.00
 
@@ -36,7 +36,7 @@ xxllist = ["nograss_xxl"]
 
 # lists are extensible, you could create a new one with your custom stuff and then add the relevant data to the lists below (name of list, radius setting, whether to skip for cutting grass)
 reftable = [skiplist,smalllist,largelist,mediumlist,xllist,xxllist]
-radiustable = [1,120,160,400,1000,2000]
+radiustable = [1,120,600,400,1000,2000]
 skiptable = [True,False,False,False,False,False]
 
 # if you want to see what decisions are made about refs set this to True, would recommend to pipe output to a text file
@@ -57,8 +57,8 @@ def is_clipping(circle_x, circle_y, rad, x, y):
     else:
         return False
 
-def is_in_list(x, ls):
-    if any(word in x for word in ls):
+def is_in_list(myrefid, reflist):
+    if any(searchterm in myrefid for searchterm in reflist):
         return True
     else:
         return False
@@ -81,7 +81,7 @@ if not os.path.isfile("tes3conv.exe"):
     print("FATAL: cannot find path to tes3conv.exe, is it in the same folder as this script?")
     sys.exit()
 
-print("Lawnmower for Morrowind",str(version)," by acidzebra: grass go brrrr")
+print("Lawnmower for Morrowind",str(version),"by acidzebra: grass go brrrr")
 
 jsonmodname = modinputfile[:-4]+".json"
 if not os.path.isfile(str(jsonmodname)):
@@ -147,33 +147,29 @@ for keys in grassfile_parsed_json:
         extcellcount+=1
         for comparekeys in modfile_parsed_json:
             if comparekeys["type"] == "Cell":
-                grasscell = keys["data"]["grid"]
-                if len(comparekeys["references"])>0 and comparekeys["data"]["grid"] == grasscell:
+                if len(comparekeys["references"])>0 and comparekeys["data"]["grid"] == keys["data"]["grid"]:
                     if moreinfo:
-                        print(grassinputfile,modinputfile,"matched cell",str(grasscell))
+                        print(grassinputfile,modinputfile,"matched cell",str(keys["data"]["grid"]))
                     matchcellcount+=1
                     for refs in keys["references"]:
-                        grasstotalcount+=1
-                        alreadymoved = False
-                        if refs["translation"][2] == -200000:
-                            alreadymoved = True
-                        if not alreadymoved:
+                        if refs["translation"][2] != -200000:
+                            grasstotalcount+=1
                             for comparerefs in comparekeys["references"]:
-                                checkthismesh = str(comparerefs["id"].casefold())
+                                checkthismesh = comparerefs["id"].casefold()
                                 matchitem = False
                                 tablecount = 0
                                 for letsref in reftable:
                                     if is_in_list(checkthismesh,reftable[tablecount]):
                                         skipitem = skiptable[tablecount]
-                                        radius = float(radiustable[tablecount])
+                                        radius = radiustable[tablecount]
                                         matchitem = True
                                         if debugradiuslist:
                                             print(skipitem,radius, checkthismesh)
+                                    tablecount+=1
                                     if matchitem:
                                         break
-                                    tablecount+=1
                                 matchitem = False 
-                                if not alreadymoved and not skipitem and is_clipping(comparerefs["translation"][0],comparerefs["translation"][1],radius,refs["translation"][0],refs["translation"][1]):
+                                if refs["translation"][2] != -200000 and not skipitem and is_clipping(comparerefs["translation"][0],comparerefs["translation"][1],radius,refs["translation"][0],refs["translation"][1]):
                                     refs["translation"][0] = 0
                                     refs["translation"][1] = 0
                                     refs["translation"][2] = -200000
